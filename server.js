3389dec034ce8fd54e09e7c18ac839b09b807edb
AbdulMoiz8994 <moiza8994@gmail.com>
2021-04-14 03:07:01 +0500

    Add thes epractice connect the mongoDBatlas with backend

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 // const dotenv = require("dotenv");
-const ConnectMongo = require("./Config/db");
+const ConnectMongo = require("./config/db");
 // dotenv.config({path : './config.env'});
 
 const app = express();
-app.use(express.json());
+// app.use(express.json());
 
 ConnectMongo();
 
@@ -15,6 +15,6 @@ app.use("/api/auth", require("./Routers/auth"));
 app.use("/api/contact", require("./Routers/contact"));
 
 // ⏬ This means is that we are on prod then it will run on envoirmental variable  otherswise in development mode it will run on develop
-const PORT = process.env.target || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`This  is Server Port number ${PORT}`));
