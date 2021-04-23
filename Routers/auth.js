3c7eb97ae310b09da2da4784543449824faeef45
AbdulMoiz8994 <moiza8994@gmail.com>
2021-04-23 22:53:43 +0500

    We have made authentication of user which chekc that user's email and password are correct or not bothmust correct

--- Routers/auth.js
+++ Routers/auth.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const {check, validationResult} =require('express-validator');
+const bcrypt=require('bcryptjs');
+const jwt=require('jsonwebtoken');
+const dotenv=require('dotenv')
+dotenv.config({path: '../config.env'})
+
+const User=require('../Model/User')
 
 //import user modle
 
@@ -7,8 +14,48 @@ router.get("/", (req, res) => {
   res.send("Get Logged in user");
 });
 
-router.post("/", (req, res) => {
-  res.send("Login the user");
+// @route Post api/auth
+// Autherize token and get user
+
+router.post("/",[
+  check("email","please enter valid email").isEmail(),
+  check("password","please enetr the password").exists()
+
+], async (req, res) => {
+ const errors= validationResult(req)
+ if(!errors.isEmpty()){
+   return res.status(400).json({errors: errors.array() })
+ } 
+ const {email,password}=req.body;
+ try{
+   
+  let user= await User.findOne({email: email})
+
+  if(!user){
+    return res.status(400).json({msg: "This user's email doest not exist"})
+  }
+ const  isMatchPass= await bcrypt.compare(password,user.password)
+if(!isMatchPass){
+  return res.status(400).json({msg: "This passowrd doest not match"})
+}
+// create payload where we save our user's id
+const payload={
+  user:{
+    id: user.id
+  }
+}
+
+jwt.sign(payload,process.env.jwtSecret,{
+  expiresIn: 3600000
+},(err,token) =>{
+     if (err) throw err;
+     res.json({token})
+})
+ }catch(err){
+    console.error(err.message)
+    res.status(500).send("Server error")
+ }
+ 
 });
 
 //we are exporting it as we do in react but syntax diff;
