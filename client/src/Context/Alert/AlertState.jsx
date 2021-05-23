e835a24a492baa97acd5016720f0343ee3320589
AbdulMoiz8994 <moiza8994@gmail.com>
2021-05-23 22:09:28 +0500

    we make the global setAlert in alertState

--- client/src/Context/Alert/AlertState.jsx
+++ client/src/Context/Alert/AlertState.jsx
@@ -2,6 +2,8 @@ import React, { useReducer } from 'react'
 import {SET_ALERT,REMOVE_ALERT} from '../Types'
 import {AlertContext} from './AlertContext'
 import {AlertReducer} from './AlertReducer'
+import {v4 as uuid} from 'uuid'
+
 
 export const AlertState = (props) => {
     // initial state is an array then if we want to make an alert then we will make an object 
@@ -10,12 +12,27 @@ export const AlertState = (props) => {
    const [state,dispatch]=useReducer(AlertReducer,initialState)
 
 // setAlert
+// we get two value in the paremeter first is alert msg and second is style type
+const setAlert=(msg,type) =>{
+    const id= 
+     dispatch({type: SET_ALERT, payload:{ msg,type,id}})
 
+// Removealert
+     setTimeout(() => dispatch({
+         type: REMOVE_ALERT,
+         payload: id
+     }),5000)
+}
 
-// Remove alert
 
     return (
         <div>
+            <AlertContext.Provider value={{
+                alert: state,
+                setAlert,
+            }}>
+                {props.children}
+            </AlertContext.Provider>
             
         </div>
     )
